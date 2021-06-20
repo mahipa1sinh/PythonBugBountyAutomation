6a5d67b6eacefc09739ff150dfd4ff72f9710cb8
mahipalsinh <nakummahipalsinh@gmail.com>
2021-06-20 16:18:24 +0530

    added fuzzing code

---
+++ DjangoDebug.py
@@ -0,0 +1,46 @@
+from starting import *
+import shodan
+import requests,re
+import os
+shodan_API_key = "okdW2znkGhdiv5jW38Z0svbHuNMoaGpQ" #enter your shodan API key
+api = shodan.Shodan(shodan_API_key) 
+words = open("bug-bounty-wordlist.txt","r")
+django_debug_list = open("django-debug-list.txt","w")
+for word in words.readlines():
+    query = "html:'URLconf defined' ssl:"+word.strip('\n')
+    try:
+        results = api.search(query)
+        print(f"Result found: {results['total']}")
+        for result in results['matches']:
+            print(word)
+            print(f"IP: {result['ip_str']}")
+            port = result['port']
+            if port in [80,443]:
+                if port==443:
+                    ip = "https://"+result['ip_str']
+                else:
+                    ip = "http://"+result['ip_str']
+            else:
+                ip = "http://"+result['ip_str']+":"+str(port)
+            django_debug_list.write(ip+'\n')
+            print('')
+    except Exception as e:
+        print(e)
+
+#automating IP address check
+
+django_debug_list = open("django-debug-list.txt","w")
+filesize = os.path.getsize("django-debug-list.txt")
+if filesize == 0:
+    print("The file is empty: " + str(filesize))#check if file is empty
+else:
+    regex = r"(?mongodb|redis):\/\/"
+    for ip in django_debug_list.readlines():
+        try:
+
+            response = requests.post(url=ip.strip("\n")+"/admin",data={},verify=False)
+            if re.search(regex,response.content):
+                print("mongodb/redis URL Found")
+
+        except Exception as e:
+            print(e)
---
+++ springboot.py
@@ -0,0 +1,37 @@
+import requests
+import shodan
+import wfuzz
+API_key_shodan = "okdW2znkGhdiv5jW38Z0svbHuNMoaGpQ" #your shodan api key goes here
+api = shodan.Shodan(API_key_shodan)
+out_file = open('spring-boot-server.txt','a')
+query = 'http.favicon.hash:116323821'
+try:
+    results = api.search(query)
+    print(f"Results found: {results['total']}")
+    for result in results['matches']:
+        print(f"IP: {result['ip_str']}"+':'+str(result['port']))
+        if result['port'] in [80,443]:
+            if result['port']==80:
+                scheme = "http://"
+            else:
+                scheme = "https://"
+            out = scheme+result['ip_str']
+        else:
+            out = "http://"+result['ip_str']+':'+str(result['port'])
+        out_file.write(out+'\n')
+except shodan.APIError as e:
+    print(f"Error: {e}")
+
+#fuzzing IP
+
+wordlist = requests.get('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/WebContent/spring-boot.txt').text.split("\n")
+springs = open("spring-boot-servers.txt","r")
+payloads = wfuzz.get_payload(wordlist)
+for spring in springs.readlines():
+    print("Fuzzing - "+spring)
+    try:
+        fuzzer = payloads.fuzz(url=spring.rstrip("\n")+"/FUZZ",sc=[200])
+        for result in fuzzer:
+            print(result)
+    except:
+        pass
