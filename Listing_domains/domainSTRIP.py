4c535c3cde85441ceaeb4b29ad588953a52c3f8f
Mahipalsinhnakum <66483110+Mahipalsinhnakum@users.noreply.github.com>
2021-07-07 10:59:15 +0530

    changed file path

--- Listing_domains/domainSTRIP.py
+++ Listing_domains/domainSTRIP.py
@@ -1,4 +1,4 @@
-domains = open("bug-bounty-domains.txt","r")
-domains2 = open("bug-bounty-domains-2.txt","w")
+domains = open("textFiles/bug-bounty-domains.txt","r")
+domains2 = open("textFiles/bug-bounty-domains-2.txt","w")
 for domain in domains.readlines():
     domains2.write(domain.lstrip('www.'))
